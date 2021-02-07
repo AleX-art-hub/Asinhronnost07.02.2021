ef09def1f0608bfca6066249b10123c5c5cdfd3c
BodNS <bondarenokn@gmail.com>
2021-02-07 12:26:25 +0200

    json

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
+/*
 const square = document.getElementById('item');
 let deg=0;
 
-/*
-setInterval(function(){
-    square.style.transform= `rotate(${++deg}deg)`
-},1000/60);
-*/
+//etInterval(function(){
+    //square.style.transform= `rotate(${++deg}deg)`
+//},1000/60);
+
 
 setInterval(rotateFun, 1000/60);
 
@@ -16,4 +16,33 @@ function rotateFun(){
 const button = document.getElementsByTagName('button')[0];
 button.onclick = function(){
     alert('Hello');
+};*/
+
+const store = {
+    users: null,
+    isFetching: false,
+    error: null,
 };
+
+const request = new XMLHttpRequest();
+
+const button = document.getElementById("loadButton");
+button.onclick = function () {
+    request.open("GET", "./data/users.json", true);
+    request.send();
+
+}
+
+request.onloadstart = function () {
+    store.isFetching = true;
+};
+
+request.onload = function () {
+    store.isFetching = false;
+    if (this.status >= 200 && this.status < 300) {
+        store.users = JSON.parse(this.responseText);
+    } else {
+        store.error = new Error(`${this.status} : ${this.statusText}`)
+    }
+};
+console.log(store);
